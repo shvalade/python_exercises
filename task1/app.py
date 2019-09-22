f4322cdc9d55ee5d48038029b4610b556925e023
shvalade <volodymyr.shakhrai@gmail.com>
2019-09-22 22:14:58 +0300

    Realise 0.1

--- task1/app.py
+++ task1/app.py
@@ -1,5 +1,37 @@
+import operator
+
+OperatorLookUp = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+    '**': operator.pow
+}
+
+
+def arithmetic(a, b, ch):
+    try:
+        return OperatorLookUp.get(ch)(a, b)
+    except ZeroDivisionError:
+        return 'Divide by zero! Program terminate'  # add exit(1)
+    except TypeError:
+        return 'Unsupported operator!'  # add exit(1)
+
+
+def get_data():
+    try:
+        a = float(input())
+        b = float(input())
+    except ValueError:
+        print('It is not integer!')
+        exit(1)
+    ch = input('Enter operator, available is : ' + OperatorLookUp.keys().__str__()[11:-2] + '\n')
+    return a, b, ch
+
+
 def main():
-    pass
+    a, b, ch = get_data()
+    print(arithmetic(a, b, ch))
 
 
 if __name__ == '__main__':
